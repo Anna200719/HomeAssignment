1f60cb3f8be91af9f5eef237e930fe71cbf0bbef
Anna200719 <73298456+Anna200719@users.noreply.github.com>
2020-10-23 19:53:54 +0300

    Added array with dummy contacts to loaders.js

---
+++ src/loaders/loaders.js
@@ -0,0 +1,11 @@
+const contacts = [
+    { name: "Aaxe Carta", role: "Internal Markets Administrator", phone: "(415) 711-1234" },
+    { name: "Ahind Rireev", role: "Product Web Assistant", phone: "(415) 711-1234" },
+    { name: "Asun Juvfhketqe", role: "Lead Intranet Coordinator", phone: "(415) 711-1234" }
+];
+
+const loadAllContacts = () => {
+    return contacts;
+}
+
+export default loadAllContacts;

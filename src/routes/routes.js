de430476ffa66a230b1bef3465ba44149c6c9fa5
Anna200719 <73298456+Anna200719@users.noreply.github.com>
2020-10-23 19:07:26 +0300

    Created route for /whos-there

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,9 @@ export default function (app) {
 
     app.get('/', function (req, res) {
         res.send('Please go to /whos-there');
-      });
+    });
+
+    app.get('/whos-there', function (req, res) {
+        res.send('Hi Trax! This is Anna\n')
+    });
 }
